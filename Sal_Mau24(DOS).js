79127bbbf5e1606450880e32813a00c40f775bf3
SalvatoreM125 <165533703+SalvatoreM125@users.noreply.github.com>
2024-05-15 10:34:26 -0400

    Add files via upload
    added buildings file

--- Sal_Mau24(DOS).js
+++ Sal_Mau24(DOS).js
@@ -14,35 +14,51 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var bagelmarker = L.marker([40.591305181408956, -74.16405412519734]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Heartland Bagels</b><br>This is a Leaflet map.").openPopup();
+bagelmarker.bindPopup(`
+    <b>Heartland Bagels</b><br> Heartland Bagels <br>
+    <img src="https://github.com/SalvatoreM125/Salvatore2/blob/main/image/heartland_bagels.avif" style="width: 100px; height: auto;">
+`).openPopup();
+
+
+
 
 
 // Add a marker to the map for New York City
 var bariosmarker = L.marker([40.58969711521726, -74.16472146976957]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Bario's Pizza</b><br>This is a Leaflet map.").openPopup();
+bariosmarker.bindPopup(`
+    <b>Bario's Pizza</b><br>Bario's Pizza <br>
+    <img src="https://github.com/SalvatoreM125/Salvatore2/blob/main/image/Barios.jpg" style="width: 100px; height: auto;">
+`).openPopup();
+
+
 
 
 // Add a marker to the map for New York City
 var traderjoesmarker = L.marker([40.589916095427704, -74.16580283820431]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Trader Joe's</b><br>This is a Leaflet map.").openPopup();
-
+traderjoesmarker.bindPopup(`
+    <b>Trader Joes</b><br> Trader Joes <br>
+    <img src="https://github.com/SalvatoreM125/Salvatore2/blob/main/image/Tradr_Joes.jfif" style="width: 100px; height: auto;">
+`).openPopup();
 
 // Add a marker to the map for New York City
 var simallmarker = L.marker([40.58194231600639, -74.16589196512663]).addTo(map);
 
 // Add a popup to the marker
-marker.bindPopup("<b>Staten Island Mall</b><br>This is a Leaflet map.").openPopup();
+simallmarker.bindPopup(`
+    <b>Staten Island Mall</b><br> Staten Island Mall <br>
+    <img src="https://github.com/SalvatoreM125/Salvatore2/blob/main/image/SImall.jpg" style="width: 100px; height: auto;">
+`).openPopup();
 
 
 
 
 
 // Load the GeoJSON line file
-fetch('https://Salvatore2.github.io/Salvatore2/akdaytimeroute.geojson')
+fetch('https://SalvatoreM125.github.io/Salvatore2/akdaytimeroute.geojson')
     .then(response => SalMroutes.json())
     .then(geojson => {
         // Customize the style of the line
@@ -64,7 +80,7 @@ fetch('https://Salvatore2.github.io/Salvatore2/akdaytimeroute.geojson')
 
 
 // Load the GeoJSON line file
-fetch('https://Salvatore2.github.io/Salvatore2/akdaytimeroute.geojson')
+fetch('https://SalvatoreM125.github.io/Salvatore2/akdaytimeroute.geojson')
     .then(response => 2salmRoutes.json())
     .then(geojson => {
         // Customize the style of the line
@@ -82,3 +98,26 @@ fetch('https://Salvatore2.github.io/Salvatore2/akdaytimeroute.geojson')
     .catch(error => {
         console.error('Error loading GeoJSON file:', error);
     });
+
+
+
+// Load the GeoJSON polygon file
+fetch('https://SalvatoreM125.github.io/Salvatore2/akeveningroutebuildings.geojson')
+.then(response => response.json())
+.then(geojson => {
+    // Customize the style of the polygon
+    var polygonStyle = {
+        fillColor: 'purple', // Fill color
+        fillOpacity: 0.5, // Fill opacity
+    };
+
+    // Add the GeoJSON polygon to the map
+    L.geoJSON(geojson, {
+        style: polygonStyle
+    }).addTo(map);
+})
+.catch(error => {
+    console.error('Error loading GeoJSON file:', error);
+});
+
+
